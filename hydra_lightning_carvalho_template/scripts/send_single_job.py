8c52185c76258978d9b6305fc06dad09904056d1
AJSVB <antoine.scardigli@epfl.ch>
2022-03-01 10:45:32 +0100

    original template

---
+++ hydra_lightning_carvalho_template/scripts/send_single_job.py
@@ -0,0 +1,30 @@
+"""
+Follows Pytorch Lightning Hydra project structure
+Assumes the existence of computation module inside src.utils
+
+# Run from root folder with: python scripts/send_single_job.py
+
+
+"""
+
+from src.utils import computation
+
+
+nodes = [computation.Euler()]
+gpus = 2
+gpu_q = 4 # define job time queue (default: 4h)
+email = False # if True an email is sent when the job is concluded
+experiment = 'example' # overwrite experiment name for logging
+name = 'resnet50_trainAB_testB'  # name of job (only for cluster)
+gpu_model = 'GeForceRTX2080Ti'#'TeslaV100_SXM2_32GB' (https://scicomp.ethz.ch/wiki/Using_the_batch_system#GPU)
+run_script = 'train.py' #train.py test.py
+
+for i in range(nb_repetitions):
+    computation.run_experiment(nodes,
+                               gpus,
+                               gpu_q,
+                               gpu_model,
+                               email,
+                               experiment,
+                               name,
+                               run_script)
